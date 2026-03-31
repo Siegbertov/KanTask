62265e9dd171291f940d9192a6b108ed949fa6ca
Sieg Bertov <siegbertov@gmail.com>
2026-03-31 12:41:15 +0300

    build: add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
 }
 
 android {
@@ -31,6 +32,7 @@ android {
         }
     }
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -48,6 +50,19 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.androidx.material.icons.extended) // NEW ICONS
+    implementation(libs.androidx.lifecycle.viewmodel.compose) // VIEWMODEL
+    implementation(libs.androidx.appcompat) // COMPATIBILITY
+    implementation(libs.androidx.navigation.compose) // NAVIGATION
+    implementation(libs.androidx.compose.foundation) // COMPOSE CORE
+    implementation(libs.androidx.compose.animation) // ANIMATION
+    coreLibraryDesugaring(libs.desugar.jdk.libs) // COMPATIBILITY with LocalDate LocalTime
+
+    implementation(libs.androidx.room.runtime) // ROOM (DATABASE)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
