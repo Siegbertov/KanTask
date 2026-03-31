87e817eb697a8227455d5ea2150f99127e3068f3
Sieg Bertov <siegbertov@gmail.com>
2026-03-31 14:09:09 +0300

    setup: add manual DI for DB and REPOSITORY with lazy initialization

---
+++ app/src/main/java/com/s1g1/kantask/KanTaskApp.kt
@@ -0,0 +1,20 @@
+package com.s1g1.kantask
+
+import android.app.Application
+import androidx.room.Room
+import com.s1g1.kantask.database.AppDatabase
+import com.s1g1.kantask.database.TaskRepository
+
+
+class KanTaskApp : Application(){
+
+    val db: AppDatabase by lazy{
+        Room.databaseBuilder(
+            context = this,
+            klass = AppDatabase::class.java,
+            name="tasks.db")
+            .build()
+    }
+
+    val repository by lazy { TaskRepository(dao = db.taskEntityDao()) }
+}
